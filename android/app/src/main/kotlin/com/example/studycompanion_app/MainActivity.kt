5400425f3da3c7335b3a30b1897e72d5e16977bc
NOOR AMERA SHAFINAZ BINTI MUHAMMAD AZMI <152240099+CikNoorSyafi@users.noreply.github.com>
2026-02-03 20:22:25 +0800

    Revert "Merge branch 'finazdraft' into main"
    This reverts commit ea42b37c106c6fe5cc97244a05784d0f269f40dc, reversing
    changes made to b6521a460df03186537f32b3437203e32b8468d6.

--- android/app/src/main/kotlin/com/example/studycompanion_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.studycompanion_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/user_session.dart
+++
@@ -1,6 +0,0 @@
-class UserSession {
-  static String name = "";
-  static String role = "";
-  static String email = "";
-  static String phone = "";
-}
